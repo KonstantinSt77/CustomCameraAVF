6d7bb111610a27b0d547042bde82104cca564de8
iuser <iuser@macbook-pro-jelvix.local>
2016-02-23 20:34:21 +0200

    Initial Commit

---
+++ Task3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task3
+//
+//  Created by iuser on 23.02.16.
+//  Copyright © 2016 iuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
