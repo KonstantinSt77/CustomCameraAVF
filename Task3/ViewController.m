aa376ea949eecf808b72b03251f82f632ccd9a6b
iuser <iuser@macbook-pro-jelvix.local>
2016-02-25 13:07:16 +0200

    first commit

--- Task3/ViewController.m
+++ Task3/ViewController.m
@@ -7,21 +7,125 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIView *frameForCaprure;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)takePhoto:(id)sender;
+- (IBAction)changeFlash:(bool)on;
+- (IBAction)changeCamera:(id)sender;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+AVCaptureSession *session;
+AVCaptureStillImageOutput *imageOutput;
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+#pragma mark  <AVFoundation/AVFoundation.h>
+    
+    session = [[AVCaptureSession alloc] init];
+    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+    
+    AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+    
+    if ([session canAddInput:deviceInput]) {
+        [session addInput:deviceInput];
+    }
+    
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    [previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    CALayer *rootLayer = [self.view layer];
+    [rootLayer setMasksToBounds:YES];
+    CGRect frame = self.frameForCaprure.frame;
+    [previewLayer setFrame:frame];
+    [rootLayer insertSublayer:previewLayer atIndex:0];
+    
+    imageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [imageOutput setOutputSettings:outputSettings];
+    
+    [session addOutput:imageOutput];
+    
+    [session startRunning];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
 }
+
+
+- (IBAction)takePhoto:(id)sender {
+    
+    AVCaptureConnection *videoConnection = nil;
+    for (AVCaptureConnection *connection in imageOutput.connections) {
+        for (AVCaptureInputPort *port in [connection inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo]) {
+                videoConnection = connection;
+                break;
+            }
+        }
+        if (videoConnection) {
+            break;
+        }
+    }
+    
+    
+    [imageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer) {
+            NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            UIImage *image = [UIImage imageWithData:imageData];
+            self.imageView.image = image;
+        }
+    }];
+}
+
+- (IBAction)changeFlash:(bool)on {    
+    Class captureDeviceClass = NSClassFromString(@"AVCaptureDevice");
+    if (captureDeviceClass != nil)
+    {
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if ([device hasTorch] && [device hasFlash])
+        {
+            [device lockForConfiguration:nil];
+            if (!on)
+            {
+                [device setTorchMode:AVCaptureTorchModeOn];
+                [device setFlashMode:AVCaptureFlashModeOn];
+                //torchIsOn = YES;
+            }
+            else
+            {
+                [device setTorchMode:AVCaptureTorchModeOff];
+                [device setFlashMode:AVCaptureFlashModeOff];
+                //torchIsOn = NO;
+            }
+            [device lockForConfiguration:nil];
+            [device unlockForConfiguration];
+        }
+    }
+}
+
+- (IBAction)changeCamera:(id)sender {
+//    if ()
+//    {
+//        FrontCamera = YES;
+//        [self viewWillAppear];
+//    } else {
+//        FrontCamera = NO;
+//        [self initCamera];
+//    }
+
+}
+
 
 @end
